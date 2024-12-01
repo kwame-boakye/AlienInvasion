37945ab22bac84a8520cead154644786f4d00957
Kelvin <[boakyekelvin306@gmail.com]>
2024-11-30 21:29:29 -0600

    Detect collision between ship and alien

--- alien_game.py
+++ alien_game.py
@@ -29,6 +29,7 @@ class AlienInvasion:
            self._check_events()
            self.ship.update()
            self._update_bullets()
+           self._update_aliens()
            self._update_screen()
 
     def _check_events(self):
@@ -70,10 +71,36 @@ class AlienInvasion:
         #Update bullet position
         self.bullets.update()
 
+        # Check for any bullets that have hit aliens
+        # If so, get rid of the bullet and the alien
+
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet
+            self.bullets.empty()
+            self._create_fleet()
+
         # Get rid of bullets that have disappeared
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions"""
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+
+    def _update_aliens(self):
+        """Check if the fleet is at an edge,then update the positions of all aliens in the fleet."""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+        # Look for alien-ship collisions
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
@@ -104,6 +131,19 @@ class AlienInvasion:
             alien.rect.x = alien.x
             alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
             self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """update images on the screen, and flip to the new screen"""
